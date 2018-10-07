90ece3986b45e09082fd3fdf11ee2836f5e245f7
Kosta Buda <kostab.work@gmail.com>
2018-10-07 05:13:47 +0300

    Home page description

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,1 +1,12 @@
 <h2><?= $title ?></h2>
+
+
+<div class="jumbotron">
+  <h1 class="display-3">Hello, world!</h1>
+  <p class="lead">Home Page if you need the Statistics, you have it on the Navigation Bar.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
